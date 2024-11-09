7f6ea0ef8298e8cb68397bce1744381f47ea2cbc
MrQuuzar <solyanov.danil.2006@gmail.com>
2024-11-10 07:48:55 +0900

    Revert "Добавление векторов"
    This reverts commit 0c72ae7ca60b4e4ba67d461cd271d85bdeb9a82b.

--- graphics engine/Vector.cs
+++ graphics engine/Vector.cs
@@ -8,65 +8,16 @@ namespace graphics_engine
 {
     internal class Vector
     {
-        public Vector()
-        {
-            this.x = 0;
-            this.y = 0;
-        }
-        public Vector(double x=0, double y=0):base()
+        public Vector(double x=0, double y=0)
         {
             this.x = x;
             this.y = y;
         }
-
         protected double x;
         protected double y;
 
         public double[] Point() => new[] { x, y };
 
-        /// <summary>
-        /// Клонирует объект Vector
-        /// </summary>
-        public Vector Clone()
-        {
-            try
-            {
-                return new Vector(x, y);
-            }
-            catch
-            {
-                return new Vector(1f, 1f);
-            }
-        }
-
-        public static implicit operator Vector(double _)
-        {
-            return new Vector(_, _);
-        }
-
-        public double this[int i]
-        {
-            get
-            {
-                switch(i)
-                {
-                    case 0: return x;
-                    case 1: return y;
-                    default: return 0;
-                }
-            }
-            set
-            {
-                switch (i)
-                {
-                    case 0: 
-                        x = value; 
-                        break;
-                    case 1: 
-                        y = value; 
-                        break;
-                }
-            }
-        }
+        public virtual Vector Clone(double x, double y) => new Vector(x, y);
     }
 }
