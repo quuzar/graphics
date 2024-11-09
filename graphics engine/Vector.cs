a361986e16f87099f702c3619432f3835cd6c7b7
MrQuuzar <solyanov.danil.2006@gmail.com>
2024-11-10 06:47:16 +0900

    Добавьте файлы проекта.

---
+++ graphics engine/Vector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graphics_engine
+{
+    internal class Vector
+    {
+        public Vector(double x=0, double y=0)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        protected double x;
+        protected double y;
+
+        public double[] Point() => new[] { x, y };
+
+        public virtual Vector Clone(double x, double y) => new Vector(x, y);
+    }
+}
